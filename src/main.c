1d52f6f54419fb5dcf0b9d1097f0c75232afc59a
Ananyo Bhattacharya <ananyobhattacharya10@gmail.com>
2026-03-31 22:48:54 +0530

    add piece creation helpers

--- src/main.c
+++ src/main.c
@@ -2,7 +2,7 @@
 
 typedef enum piece_colour
 {
-    NO_COLOR,
+    NO_COLOUR,
     WHITE,
     BLACK
 } PC;
@@ -23,6 +23,28 @@ typedef struct piece
     PC colour;
     PT type;
 } Piece;
+
+// --- Piece Creation Helpers ------------>
+
+static inline Piece make_piece(PC colour, PT type)
+{
+    return (Piece){colour, type};
+}
+
+static inline Piece no_piece(void)
+{
+    return make_piece(NO_COLOUR, NO_PIECE);
+}
+
+static inline Piece white(PT type)
+{
+    return make_piece(WHITE, type);
+}
+
+static inline Piece black(PT type)
+{
+    return make_piece(BLACK, type);
+}
 
 // --- Board ------------>
 
