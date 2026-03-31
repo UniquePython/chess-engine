7f88223d4d313474269cf9c02980370954053140
Ananyo Bhattacharya <ananyobhattacharya10@gmail.com>
2026-03-31 23:40:37 +0530

    create function to initialize board

--- src/main.c
+++ src/main.c
@@ -158,3 +158,38 @@ typedef struct
     bool is_castle;
     bool is_en_passant;
 } Move;
+
+// --- Core logic ------------>
+
+static inline void init_board(Board *b)
+{
+    for (int i = 0; i < 64; i++)
+        b->squares[i] = no_piece();
+
+    // Pawns
+    for (File f = A; f <= H; f++)
+    {
+        set(b, (Loc){TWO, f}, white(PAWN));
+        set(b, (Loc){SEVEN, f}, black(PAWN));
+    }
+
+    // White back rank
+    set(b, (Loc){ONE, A}, white(ROOK));
+    set(b, (Loc){ONE, B}, white(KNIGHT));
+    set(b, (Loc){ONE, C}, white(BISHOP));
+    set(b, (Loc){ONE, D}, white(QUEEN));
+    set(b, (Loc){ONE, E}, white(KING));
+    set(b, (Loc){ONE, F}, white(BISHOP));
+    set(b, (Loc){ONE, G}, white(KNIGHT));
+    set(b, (Loc){ONE, H}, white(ROOK));
+
+    // Black back rank
+    set(b, (Loc){EIGHT, A}, black(ROOK));
+    set(b, (Loc){EIGHT, B}, black(KNIGHT));
+    set(b, (Loc){EIGHT, C}, black(BISHOP));
+    set(b, (Loc){EIGHT, D}, black(QUEEN));
+    set(b, (Loc){EIGHT, E}, black(KING));
+    set(b, (Loc){EIGHT, F}, black(BISHOP));
+    set(b, (Loc){EIGHT, G}, black(KNIGHT));
+    set(b, (Loc){EIGHT, H}, black(ROOK));
+}
